b50d642ed8df9d98a50cc7fd0ad90ebeb93c4d2c
Rofayda <roufybassem@gamil.com>
2023-05-17 23:54:21 +0300

    very small change

--- src/main/java/Indexer/QueryProcessing.java
+++ src/main/java/Indexer/QueryProcessing.java
@@ -40,11 +40,12 @@ public class QueryProcessing {
     boolean isValidSearch;
     double startTime;
     double endTime;
-    public QueryProcessing(String stringToSearch){
+
+    public QueryProcessing(String stringToSearch) {
         this.dbManager = new DataBaseManager();
         this.currentStringToSearch = stringToSearch;
         this.stemmer = new PorterStemmer();
-        this.rankingWordsInSearch =  new HashMap<>();
+        this.rankingWordsInSearch = new HashMap<>();
         this.rankingResults = new HashMap<>();
         this.returnedJsonArray = new JSONArray();
         this.correctResults = new Vector<>();
@@ -54,39 +55,40 @@ public class QueryProcessing {
         //execute the queryProcessing functionality directly from the constructor
         buildStoppingWord();
         startTime = System.currentTimeMillis();
-        if(processSearchString()){
+        if (processSearchString()) {
             rankSearchWords();
             isValidSearch = true;
             processTheRankerResult();
-        }
-        else{
+        } else {
             isValidSearch = false;
         }
         endTime = System.currentTimeMillis();
     }
-    public boolean processSearchString(){
+
+    public boolean processSearchString() {
 
         String s = dataPreProcessing(this.currentStringToSearch);
         this.searchWordsList = List.of(s.split(" "));
         this.searchWordsList = removeStoppingWord(this.searchWordsList);
-        for(int i = 0; i < this.searchWordsList.size(); i++){
+        for (int i = 0; i < this.searchWordsList.size(); i++) {
             String w = this.searchWordsList.get(i);
             w = stemmer.stem(w);
-            if(w.length() > 2)
+            if (w.length() > 2)
                 this.stemmedSearchWordsList.add(w);
         }
-        if(this.stemmedSearchWordsList.isEmpty())
+        if (this.stemmedSearchWordsList.isEmpty())
             return false;
         rankingWordsInSearch = dbManager.getWordsDataCollection().getWordToSearch(this.stemmedSearchWordsList);
         return true;
     }
 
-    public void rankSearchWords(){
+    public void rankSearchWords() {
         ranker = new Ranker(this.dbManager, rankingWordsInSearch);
         ranker.CalculateRelevance();
         rankingResults = ranker.sortUrls();
     }
-    public String dataPreProcessing(String s){
+
+    public String dataPreProcessing(String s) {
         // \W, is equivalent to [^a-zA-Z0-9] regular expression
         // replace any non-word character with spaces
         s = s.replaceAll("[\\W\\d]", " ");
@@ -99,7 +101,8 @@ public class QueryProcessing {
 
 
     }
-    public void buildStoppingWord(){
+
+    public void buildStoppingWord() {
         stoppingWord = new Trie();
         String stoppingWordDictionaryFilePath = "./src/main/java/Indexer/stoppingWords.txt";
 
@@ -114,23 +117,23 @@ public class QueryProcessing {
             e.printStackTrace();
         }
     }
-    public boolean isStoppingWord(String s){
+
+    public boolean isStoppingWord(String s) {
 
         return stoppingWord.search(s);
     }
-    public List<String> removeStoppingWord(List<String> wordsWithStoppingWords){
-        List <String> wordsWithoutStoppingWords = new ArrayList<>();
-        for(String word : wordsWithStoppingWords)
-        {
-            if(!isStoppingWord(word))
-            {
+
+    public List<String> removeStoppingWord(List<String> wordsWithStoppingWords) {
+        List<String> wordsWithoutStoppingWords = new ArrayList<>();
+        for (String word : wordsWithStoppingWords) {
+            if (!isStoppingWord(word)) {
                 wordsWithoutStoppingWords.add(word);
             }
         }
         return wordsWithoutStoppingWords;
     }
 
-    public boolean containAnd(){
+    public boolean containAnd() {
         // Create the regular expression pattern
         String pattern = "\"([^\"]+)\"\\s+and\\s+\"([^\"]+)\"";
         return extractPattern(pattern);
@@ -142,34 +145,33 @@ public class QueryProcessing {
         return extractPattern(pattern);
     }
 
-    public boolean containNot(){
+    public boolean containNot() {
         // Create the regular expression pattern
         String pattern = "\"([^\"]+)\"\\s+not\\s+\"([^\"]+)\"";
         return extractPattern(pattern);
     }
 
-    public boolean extractPattern(String pattern){
+    public boolean extractPattern(String pattern) {
         // Create a Pattern object
         Pattern regex = Pattern.compile(pattern);
         // Create a Matcher object
         Matcher matcher = regex.matcher(this.currentStringToSearch);
 
         // Check if the input string matches the pattern
-        if(matcher.find())
-        {
+        if (matcher.find()) {
             this.extractedString1 = matcher.group(1);
             this.extractedString2 = matcher.group(2);
             return true;
-        }
-        else
+        } else
             return false;
     }
-    public void processTheRankerResult(){
+
+    public void processTheRankerResult() {
 
         //check if phraseSearching
-        if(this.currentStringToSearch.startsWith("\"") && this.currentStringToSearch.endsWith("\"")){
+        if (this.currentStringToSearch.startsWith("\"") && this.currentStringToSearch.endsWith("\"")) {
             //check for and
-            if(containAnd()){
+            if (containAnd()) {
                 List<WordData> l1 = phraseSearch(this.extractedString1);
                 List<WordData> l2 = phraseSearch(this.extractedString2);
                 // Combine the 2 list
@@ -181,7 +183,7 @@ public class QueryProcessing {
                 this.correctResults.addAll(set);
             }
             //check for or
-            else if(containOr()){
+            else if (containOr()) {
                 List<WordData> l1 = phraseSearch(this.extractedString1);
                 List<WordData> l2 = phraseSearch(this.extractedString2);
                 // Get the intersection of the two lists
@@ -190,39 +192,38 @@ public class QueryProcessing {
 
             }
             //check for not
-            else if(containNot()){
+            else if (containNot()) {
                 List<WordData> l1 = phraseSearch(this.extractedString1);
                 List<WordData> l2 = phraseSearch(this.extractedString2);
                 //exclude l2 from l1
                 l1.removeAll(l2);
                 this.correctResults = l1;
-            }
-            else {
+            } else {
                 this.correctResults = phraseSearch(this.currentStringToSearch.substring(1, this.currentStringToSearch.length() - 1));
             }
             //No document have the same exact match
-            if(this.correctResults.size() == 0){
+            if (this.correctResults.size() == 0) {
                 this.isValidSearch = false;
             }
-        }
-        else{
+        } else {
             originalSearch();
         }
     }
 
-    public void originalSearch(){
+    public void originalSearch() {
         this.correctResults.addAll(rankingResults.keySet());
     }
-    public void createJsonObject(){
+
+    public void createJsonObject() {
 
         //the JSON object has the following fields
         // "search Text":"how to solve your bug in 3 days only"
 //         "title":"solving bugs"
 //         "URL":"programmingGeeksGeek.com"
 //         "paragraph":"100 coffee cup + 2 hours sleep/day + lots of cry + stack overflow = solved bug in 3 days"
-        for(WordData url : this.correctResults){
+        for (WordData url : this.correctResults) {
             JSONObject jsonObject = new JSONObject();
-            if(url.url == null)
+            if (url.url == null)
                 continue;
             jsonObject.put("title", getPageTitle(url));
             jsonObject.put("URL", url.url);
@@ -233,20 +234,20 @@ public class QueryProcessing {
         this.dbManager.getHistoryDataCollection().InsertHistory(this.returnedJsonArray, this.currentStringToSearch, 1);
     }
 
-    public JSONArray getResultJSONList(){
-        if(isValidSearch) {
+    public JSONArray getResultJSONList() {
+        if (isValidSearch) {
             createJsonObject();
             return this.returnedJsonArray;
-        }
-        else
+        } else
             return new JSONArray(); //this will return an empty json array
     }
 
-    public double getSearchTime(){
+    public double getSearchTime() {
 
         return endTime - startTime;
     }
-    public String getPageTitle(WordData urlData){
+
+    public String getPageTitle(WordData urlData) {
 
         String filePath = urlData.filepath;
         String pageTitle = "";
@@ -257,7 +258,7 @@ public class QueryProcessing {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return  pageTitle;
+        return pageTitle;
     }
 
     public String getParagraph(WordData urlData) {
@@ -282,8 +283,7 @@ public class QueryProcessing {
 
                     // Check if any of the strings in the list are present in the element
                     for (String searchString : this.stemmedSearchWordsList) {
-                        if (elementText.toLowerCase().contains(searchString.toLowerCase()))
-                        {
+                        if (elementText.toLowerCase().contains(searchString.toLowerCase())) {
                             pageParagraph = elementText;
                             break;
                         }
@@ -303,19 +303,19 @@ public class QueryProcessing {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return  pageParagraph;
+        return pageParagraph;
     }
 
-    public List<WordData> phraseSearch(String searchString){
+    public List<WordData> phraseSearch(String searchString) {
         //iterate over all the returned urls coming from the ranker i.e. this.rankingResults
         //get the first element that has the same word as the phraseSearch and start comparing from here
         //if they are the same add it to this array (this.correctResults), if not then continue
         //NOTE: if no web pages are found, set the this.isValidSearch = false;
         List<WordData> correctRes = new ArrayList<>();
-        for(Map.Entry<WordData, Double> entry : this.rankingResults.entrySet()){
+        for (Map.Entry<WordData, Double> entry : this.rankingResults.entrySet()) {
             WordData urlData = entry.getKey();
             String filePath = urlData.filepath;
-            if(filePath == null)
+            if (filePath == null)
                 continue;
             try {
                 Document currentDoc = Jsoup.parse(new File(filePath), "UTF-8");
@@ -326,25 +326,24 @@ public class QueryProcessing {
                 int docWordsIndex = 0;
                 int searchWordsLength = searchWords.length;
                 int docWordsLength = docWords.length;
-                while(searchWordsIndex < searchWordsLength && docWordsIndex < docWordsLength){
-                    if(searchWords[searchWordsIndex].equalsIgnoreCase(docWords[docWordsIndex])){
+                while (searchWordsIndex < searchWordsLength && docWordsIndex < docWordsLength) {
+                    if (searchWords[searchWordsIndex].equalsIgnoreCase(docWords[docWordsIndex])) {
                         searchWordsIndex++;
                         docWordsIndex++;
-                    }
-                    else{
+                    } else {
                         searchWordsIndex = 0;
                         docWordsIndex++;
                     }
 
 
                 }
-                if(searchWordsIndex == searchWordsLength){
+                if (searchWordsIndex == searchWordsLength) {
                     correctRes.add(urlData);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
+        return correctRes;
     }
 }
